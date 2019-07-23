8831feef063d2d481f04a7acae3a42534f009123
Swami Anil Saraswati <swamianilsaraswati@gmail.com>
2019-07-23 16:26:26 +0530

    Initial commit.

---
+++ src/main/java/com/amdocs/amdocshellorestapi/Application.java
@@ -0,0 +1,13 @@
+package com.amdocs.amdocshellorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
